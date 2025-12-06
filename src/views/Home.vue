0fcd70c87753513a16bb5742a8e421c7d8e1de04
eldovyn <farras.pramudita@gmail.com>
2025-12-06 22:11:58 +0700

    chore: add sample note list data

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -14,11 +14,21 @@ import { Label } from '@/components/ui/label';
 import { Textarea } from '@/components/ui/textarea';
 import { Separator } from '@/components/ui/separator';
 import { BxRegularSave } from 'vue-icons-lib/bx';
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle,
+} from '@/components/ui/card';
+
+const cards = Array.from({ length: 12 }, (_, i) => i + 1);
 </script>
 
 <template>
     <div class="h-screen w-full">
-        <div class="flex justify-between pe-6 ps-6 mt-10">
+        <div class="flex justify-between pe-2 ps-6 mt-10">
             <div class="relative w-70">
                 <Input placeholder="Search your notes" class="w-full pl-12" />
 
@@ -55,6 +65,21 @@ import { BxRegularSave } from 'vue-icons-lib/bx';
                     </form>
                 </DialogContent>
             </Dialog>
+        </div>
+        <br>
+        <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 pe-2 ps-6">
+            <Card v-for="card in cards" :key="card" class="border rounded-lg shadow-sm">
+                <CardHeader>
+                    <CardTitle>Card Title {{ card }}</CardTitle>
+                    <CardDescription>Card Description {{ card }}</CardDescription>
+                </CardHeader>
+                <CardContent>
+                    <p>Card Content {{ card }}</p>
+                </CardContent>
+                <CardFooter>
+                    <p>Card Footer {{ card }}</p>
+                </CardFooter>
+            </Card>
         </div>
     </div>
 </template>
